2caedd79f0383035543c8ff47af9731303d9672a
Marcus Eckert <marcuseckert@gmail.com>
2024-11-28 18:09:53 +0100

    UPDATE

---
+++ PocketSVG.h
@@ -0,0 +1,18 @@
+//
+//  PocketSVG.h
+//  PocketSVG
+//
+//  Created by Marcus Eckert on 28.11.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PocketSVG.
+FOUNDATION_EXPORT double PocketSVGVersionNumber;
+
+//! Project version string for PocketSVG.
+FOUNDATION_EXPORT const unsigned char PocketSVGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocketSVG/PublicHeader.h>
+
+
